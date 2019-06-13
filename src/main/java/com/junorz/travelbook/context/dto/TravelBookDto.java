01e28a5016cb9ad199a9b072c04269cb983130b2
JUN <junvorz@gmail.com>
2019-06-13 16:08:21 +0900

    add settle path feature

--- src/main/java/com/junorz/travelbook/context/dto/TravelBookDto.java
+++ src/main/java/com/junorz/travelbook/context/dto/TravelBookDto.java
@@ -3,6 +3,7 @@ package com.junorz.travelbook.context.dto;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -54,8 +55,17 @@ public class TravelBookDto {
 
         // initialize settle details with member name, and set other properties to 0.
         memberList.forEach(m -> calculation.settleDetails
-                .add(new SettleDetail(m.getName(), BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO)));
-
+                .add(new SettleDetail(m.getName(), BigDecimal.ZERO, BigDecimal.ZERO, new ArrayList<Path>())));
+        
+        // initialize path list
+        calculation.settleDetails.forEach(s -> {
+            memberList.forEach(m -> {
+                if (!m.getName().equals(s.getName())) {
+                    s.getPaths().add(new Path(m.getName()));
+                }
+            });
+        });
+        
         detailList.forEach(d -> {
             // calculate paid amount.
             for (SettleDetail s : calculation.settleDetails) {
@@ -70,20 +80,62 @@ public class TravelBookDto {
             BigDecimal average = new BigDecimal(d.getAmount()).divide(new BigDecimal(memberCount), 10,
                     RoundingMode.HALF_UP);
             d.getMemberList().forEach(m -> {
-                for (SettleDetail s : calculation.settleDetails) {
+                calculation.settleDetails.forEach(s -> {
                     if (m.getName().equals(s.getName())) {
                         s.setAverage(s.getAverage().add(average));
-                        break;
+                        
+                        // set settle path
+                        // Conditions:
+                        // 1. If the member does not the payer.
+                        // 2. The name in settle details matches.
+                        if (!m.getName().equals(d.getMember().getName())) {
+                            s.getPaths().forEach(p -> {
+                                if (p.getToName().equals(d.getMember().getName())) {
+                                    p.setAmount(p.getAmount().add(average));
+                                }
+                            });
+                        }
                     }
-                }
+                });
             });
+            
         });
         
-        // calculate balance
+        // Drop path with 0 amount.
         calculation.settleDetails.forEach(s -> {
-            s.setBalance(s.getPaid().subtract(s.getAverage()));
+            Iterator<Path> it = s.getPaths().iterator();
+            while(it.hasNext()) {
+                Path p = it.next();
+                if (p.getAmount().equals(BigDecimal.ZERO)) {
+                    it.remove();
+                }
+            }
         });
-
+        
+        // Make the settle path more intelligent (BETA)
+        calculation.settleDetails.forEach(s -> {
+            Iterator<Path> itPath1 = s.getPaths().iterator();
+            while (itPath1.hasNext()) {
+                Path p1 = itPath1.next();
+                calculation.settleDetails.forEach(s2 -> {
+                    if(p1.getToName().equals(s2.getName())) {
+                        Iterator<Path> itPath2 = s2.getPaths().iterator();
+                        while (itPath2.hasNext()) {
+                            Path p2 = itPath2.next();
+                            if(p2.getToName().equals(s.getName())) {
+                                if (0 < p1.getAmount().compareTo(p2.getAmount())) {
+                                    p1.setAmount(p1.getAmount().subtract(p2.getAmount()));
+                                    itPath2.remove();
+                                } else {
+                                    p2.setAmount(p2.getAmount().subtract(p1.getAmount()));
+                                    itPath1.remove();
+                                }
+                            }
+                        }
+                    }
+                });
+            } 
+        });
         return this;
     }
 
@@ -93,7 +145,7 @@ public class TravelBookDto {
         private BigDecimal amount;
 
         // details of payers
-        // a list of { "name": xxx, "paid": xxx, "average": xxx }
+        // a list of { "name": xxx, "paid": xxx, "average": xxx, "balance": xxx, "path": xxx }
         private List<SettleDetail> settleDetails = new ArrayList<>();
 
     }
@@ -104,7 +156,20 @@ public class TravelBookDto {
         private String name;
         private BigDecimal paid;
         private BigDecimal average;
-        private BigDecimal balance;
+        private List<Path> paths;
+    }
+    
+    @Data
+    private static class Path {
+        
+        public Path(String toName) {
+            this.toName = toName;
+        }
+        
+        // pay to whom
+        private String toName;
+        // amount need to pay
+        private BigDecimal amount = BigDecimal.ZERO;
     }
 
 }
